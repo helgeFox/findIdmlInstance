edd83a51b504f4f73732ff3e12af48731cc180cc
Helge Hofstad <helge@foxpublish.com>
2018-05-26 00:32:05 +0200

    simplify main.js file

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const getPdf = require('./get-pdf-from-guid');
-const getIdml = require('./get-idml-from-guid');
-
 exports = module.exports = {
-	getPdfFromGuid: getPdf,
-	getIdmlFromGuid: getIdml
+	getPdfFromGuid: require('./get-pdf-from-guid'),
+	getIdmlFromGuid: require('./get-idml-from-guid')
 };
